64ac31aff9edefc37604bc5118ab51653bc69454
casperlamboo <casperlamboo@gmail.com>
2016-03-18 12:47:18 +0100

    update kcm texture

--- LoaderTests/kcm_texture.js
+++ LoaderTests/kcm_texture.js
@@ -2,122 +2,103 @@ import 'mrdoob/three.js';
 import 'mrdoob/three.js/controls/EditorControls.js';
 import GTXLoader from '/src/loaders/GTXLoader.js';
 import KCMLoader from '/src/loaders/KCMLoader.js';
-import ImageData from '/src/utils/ImageData.js';
 
-let loaderKCM = new KCMLoader();
-let loaderGTX = new GTXLoader();
-loaderKCM.load('/data/MAPS/n_031_031.kcm').then(async ({textureMaps}) => {
+const TEXTURE_WIDTH = 8192;
+const TEXTURE_HEIGHT = 8192;
 
-	let colorMap = [];
+const canvas = document.createElement('canvas');
+canvas.width = TEXTURE_WIDTH;
+canvas.height = TEXTURE_HEIGHT;
+const context = canvas.getContext('2d');
 
-	for (let {alphaMap, textureID, firstLayer} of textureMaps) {
+document.body.appendChild(canvas);
 
-		let textureUrl = `../data/MAPS/Tex/b_00${textureID + 1}.GTX`;
+const loaderKCM = new KCMLoader();
+const loaderGTX = new GTXLoader();
+loaderKCM.load('/data/MAPS/n_031_031.kcm', async ({ textureMaps }) => {
+	let first = true;
+	for (const { alphaMap, textureID, firstLayer } of textureMaps) {
+		const textureUrl = `../data/MAPS/Tex/b_00${textureID + 1}.GTX`;
 
-		let map = await new Promise((resolve, reject) => {
-			let map = loaderGTX.load(textureUrl, () => {
-				resolve(map);
-			}, reject);
+		const texture = await new Promise((resolve, reject) => {
+			const texture = loaderGTX.load(textureUrl, () => {
+				resolve(texture);
+			}, undefined, reject);
 		});
 
-		let imageData = textureToImageData(map);
+		const textureImage = textureToImage(texture);
+		const texturePattern = context.createPattern(textureImage, 'repeat');
+		const alphaImage = createAlphaMap(alphaMap, 256, 256);
 
-		for (let x = 0; x < imageData.width; x ++) {
-			for (let y = 0; y < imageData.height; y ++) {
-				let i = y * imageData.width + x;
+		if (first) {
+			context.fillStyle = texturePattern;
+			context.fillRect(0, 0, TEXTURE_WIDTH, TEXTURE_HEIGHT);
 
-				let color = imageData.getPixel(x, y);
+			first = false;
+		} else {
 
-				if (firstLayer) {
-					colorMap[i] = color;
-				}
-				else {
-					let _i = Math.floor(y/2) * imageData.width / 2 + Math.floor(x/2);
+			const image = combineAlphaPattern(alphaImage, texturePattern, TEXTURE_WIDTH, TEXTURE_HEIGHT);
 
-					let alpha = alphaMap[_i];
-
-					color.a = alpha;
-
-					colorMap[i].a = alpha / 256;
-
-					// colorMap[i] = colorMap[i].blend(color);
-				}
-			}
+			context.drawImage(image, 0, 0);
 		}
 	}
-
-	let texture = new ImageData(512, 512);
-
-	for (let x = 0; x < texture.width; x ++) {
-		for (let y = 0; y < texture.height; y ++) {
-			let i = y * texture.width + x;
-			let color = colorMap[i];
-
-			texture.setPixel(color, x, y);
-		}
-	}
-
-	let canvas = document.getElementById('canvas');
-	let context = canvas.getContext('2d');
-
-	let imageData = context.getImageData(0, 0, texture.width, texture.height);
-	for (let i = 0; i < imageData.data.length; i ++) {
-		imageData.data[i] = texture.data[i];
-	}
-
-	context.putImageData(imageData, 0, 0);
 });
 
 function createAlphaMap (map, width, height) {
-	let canvas = document.createElement('canvas');
-	canvas.width = 256;
-	canvas.height = 256;
-	let context = canvas.getContext('2d');
+	const canvas = document.createElement('canvas');
+	canvas.width = width;
+	canvas.height = height;
+	const context = canvas.getContext('2d');
+
+	const imageData = context.getImageData(0, 0, width, height);
 
 	for (let x = 0; x < width; x ++) {
 		for (let y = 0; y < height; y ++) {
-			let i = y * canvas.width + x;
-			let color = map[i];
-			context.fillStyle = `rgb(${color}, ${color}, ${color})`;
-			context.fillRect(x, y, 1, 1);
+			const index = y * width + x;
+			const color = map[index];
+
+			imageData.data[index * 4 + 3] = color;
 		}
 	}
+
+	context.putImageData(imageData, 0, 0);
 
 	return canvas;
 }
 
-function textureToImageData (texture) {
-	let {width, height} = texture.image;
+function textureToImage (texture) {
+	const { width, height } = texture.image;
 
-	let webGLCanvas = document.createElement('canvas');
-	webGLCanvas.width = width;
-	webGLCanvas.height = height;
+	const renderer = new THREE.WebGLRenderer();
+	renderer.setSize(width, height);
 
-	let renderer = new THREE.WebGLRenderer({
-	    preserveDrawingBuffer: true, 
-		canvas: webGLCanvas
-	});
+	const scene = new THREE.Scene();
 
-	let scene = new THREE.Scene();
-
-	let geometry = new THREE.PlaneGeometry(width, height, 1, 1);
-	let material = new THREE.MeshBasicMaterial({map: texture});
-	let plane = new THREE.Mesh(geometry, material);
+	const geometry = new THREE.PlaneGeometry(width, height, 1, 1);
+	const material = new THREE.MeshBasicMaterial({ map: texture });
+	const plane = new THREE.Mesh(geometry, material);
 
 	scene.add(plane);
 
-	let camera = new THREE.OrthographicCamera(width / -2, width / 2, height / 2, height / -2, 0, 1);
+	const camera = new THREE.OrthographicCamera(width / -2, width / 2, height / 2, height / -2, 0, 1);
 
 	renderer.render(scene, camera);
 
-	let canvas = document.createElement('canvas');
+	return renderer.domElement;
+}
+
+function combineAlphaPattern(alpha, pattern, width, height) {
+	const canvas = document.createElement('canvas');
 	canvas.width = width;
 	canvas.height = height;
-	let context = canvas.getContext('2d');
+	const context = canvas.getContext('2d');
 
-	context.drawImage(webGLCanvas, 0, 0);
+	context.drawImage(alpha, 0, 0, width, height);
 
-	let imageData = new ImageData(context);
+	context.globalCompositeOperation = 'source-in';
 
-	return imageData;
+	context.fillStyle = pattern;
+	context.fillRect(0, 0, width, height);
+
+	return canvas;
 }
