6caa450d61d6c797ae327681be941abba8162b4d
casperlamboo <casperlamboo@gmail.com>
2016-05-16 11:50:03 +0200

    simplify utils

--- src/utils/Utils.js
+++ src/utils/Utils.js
@@ -1,19 +1,19 @@
-export let MathExtended = {
-	clamb: function (value, min, max) {
+export const MathExtended = {
+	clamb(value, min, max) {
 		return (value > min) ? ((value < max) ? value : max) : min;
 	},
-	randomInt: function (min, max) {
+	randomInt(min, max) {
 		return Math.floor(MathExtended.random(min, max + 1));
 	},
-	random: function (min = 0, max = 1) {
+	random(min = 0, max = 1) {
 		return Math.random() * (max - min) + min;
 	},
-	sign: function (value) {
+	sign(value) {
 		return (value > 0) ? 1 : ((value < 0) ? -1 : 0);
 	}
 };
 
-export let padStr = (str, length) => {
+export const padStr = (str, length) => {
 	str = String(str);
 	while (str.length < length) {
 		str = `0${str}`;
