3cb32e4aa72fcdb69f79d6ddfcbe5cecb586f40c
casperlamboo <casperlamboo@gmail.com>
2017-03-05 12:14:14 +0100

    convert black to alpha

--- src/plugins/bmp.js
+++ src/plugins/bmp.js
@@ -7,6 +7,14 @@ const CONTEXT = CANVAS.getContext('2d');
 export async function fetch(load) {
   const [imageData] = await pixelBitmap.parse(load.address);
 
+  for (let i = 0; i < imageData.data.length; i += 4) {
+    if (imageData.data[i] !== 0) continue;
+    if (imageData.data[i + 1] !== 0) continue;
+    if (imageData.data[i + 2] !== 0) continue;
+
+    imageData.data[i + 3] = 0;
+  }
+
   CANVAS.width = imageData.width;
   CANVAS.height = imageData.height;
   CONTEXT.putImageData(imageData, 0, 0);
